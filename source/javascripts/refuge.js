2b68a8108e6e3cf3c38e0cb21dda651e54fabeca
fairwinds <fairwinds.dp@gmail.com>
2012-07-18 12:57:06 -0300

    add customizations

---
+++ source/javascripts/refuge.js
@@ -0,0 +1,9 @@
+/* 
+* Refuge JS customizations
+*
+* Copyright and Attribution:
+* The following copyright and attribution applies to this document:
+* Copyright (c) 2012 David Pratt. All rights reserved.
+* CONTRIBUTORS:
+*   David Pratt
+*/
